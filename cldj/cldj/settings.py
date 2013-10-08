bd0f25c78172eecaa33976d39d6cb75802d5203a
Chris Miller <chris@chrismiller.org>
2013-10-08 12:35:34 -0500

    Initial PostgreSQL localhost config

--- cldj/cldj/settings.py
+++ cldj/cldj/settings.py
@@ -57,8 +57,11 @@ WSGI_APPLICATION = 'cldj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cldjdev',
+        'USER': 'cldj',
+        'PASSWORD': 'UcBitNog^',
+        'HOST': '127.0.0.1',
     }
 }
 
