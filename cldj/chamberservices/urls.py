e2b468c36a37d57701bd4e9fa98d310b7f737514
Chris Miller <chris@chrismiller.org>
2013-12-20 17:54:57 -0600

    Chamber services module development waypoint.

---
+++ cldj/chamberservices/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('chamberservices.views',    
+    url(r'^request-quote/$', 'request_proposal', name='request_proposal'),
+)
